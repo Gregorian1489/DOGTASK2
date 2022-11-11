718fcc80242153d2dff51b48799c14ba4296ad42
Gregorian1489 <santijago@mail.ru>
2022-11-11 15:46:53 +0400

    Доработан вариант с выбором скоростей, решение через условный оператор

---
+++ Version2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите дистанцию (больше 10)");
+int distance = Convert.ToInt32(Console.ReadLine());          //Преобразовываем строчный тип в число и записываем в переменную
+Console.WriteLine("Введите ОТ какого друга побежит собака (1 или 2) ");
+int friend  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость первого друга");
+int speed1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость второго друга");
+int speed2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость собаки");
+int dogspeed = Convert.ToInt32(Console.ReadLine());
+int time;
+int count = 0;
+
+Console.WriteLine("==Исходные данные==:");
+Console.WriteLine($"Дистанция = {distance}");
+Console.WriteLine($"Скорость первого друга = {speed1}, скорость второго друга = {speed2}");
+Console.WriteLine($"Скорость собаки = {dogspeed}, пес бежит от {friend} - го друга");
+
+while (distance>10)
+{
+    if (friend == 2)
+    {
+    time = distance/(speed1 + dogspeed);
+    distance = distance - (speed1+speed2) * time; 
+    friend = 1;
+    }
+    else
+    {
+    time = distance/(speed2 + dogspeed);
+    distance = distance - (speed1+speed2) * time;
+    friend = 2;
+    }
+    count++;
+    
+}
+Console.WriteLine("-----------------------------");
+Console.WriteLine($"Собака пробежит {count} раз(а)");
+Console.WriteLine("-----------------------------"); 
